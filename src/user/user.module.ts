9c87b075e8c272493af098b05b915e770749f311
lyntphust <plysn12092001@gmail.com>
2024-12-21 13:20:47 +0700

    Fix cart product stock

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -9,6 +9,7 @@ import { S3CoreModule } from 'src/s3/src/s3.module';
 
 import { UserService } from './user.service';
 import { ProductCart } from 'src/product/enities/product-cart.entity';
+import { ProductVariant } from 'src/product/enities/product-variant.entity';
 import { UserController } from './user.controller';
 import { BlogController } from './blog.controller';
 import { BlogService } from './blog.service';
@@ -17,7 +18,7 @@ import { Product } from 'src/product/enities/product.entity';
 import { FavoriteProduct } from 'src/product/enities/favorite-product.entity';
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, Permission, ProductCart,Blog,Product,FavoriteProduct]),
+    TypeOrmModule.forFeature([User, Permission, ProductCart, ProductVariant, Blog, Product, FavoriteProduct]),
     RoleModule,
     S3CoreModule
   ],
