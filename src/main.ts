559fd5b1a6b43e0deff2ebb4b84397ab3f968b25
lyntphust <plysn12092001@gmail.com>
2025-01-19 00:50:22 +0700

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    allowedHeaders: ['content-type'],
+    allowedHeaders: '*',
     origin: ['http://localhost:3000', 'https://camping-web-fe-beta.vercel.app'],
     credentials: true,
   });
