c1ca02e99660fcdb3bf99a741269adafa58336d2
lyntphust <plysn12092001@gmail.com>
2025-01-18 23:50:01 +0700

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,11 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+    },
+  });
 
   app.use(cookieParser());
 
